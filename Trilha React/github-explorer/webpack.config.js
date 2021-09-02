db68ec62b79f217bee14781100d775982715450e
Marco <sgtmarcoaurelio@icloud.com>
2021-09-01 22:13:41 -0300

    :rocket::rocket::rocket:

--- Trilha React/github-explorer/webpack.config.js
+++ Trilha React/github-explorer/webpack.config.js
@@ -2,6 +2,7 @@
 // Para não ter problemas em encontrar os arquivos quando estivermos em um ambiente (SO) diferente.
 const path = require('path'); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -18,18 +19,27 @@ module.exports = {
     },
     devServer: {
         static: path.resolve(__dirname, 'public'), // Deve apontar para o diretório onde estão os arquivos que serão exibidos na aplicação (HTML estático).
+        hot: true, // Habilita o hot reloading.
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(), // Para que o React Refresh funcione.
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html'), // Define qual arquivo será usado como modelo para o HTML.
         })
-    ],
+    ].filter(Boolean), // Hack para resolver o problema do ReactRefreshWebpackPlugin quando o ambiente for de produção.
     module: { // Define como os arquivos importados serão interpretados. (Um objeto para cada tipo de arquivo).
         rules: [
             {
                 test: /\.jsx$/, // Expressão regular para verificar e encontrar arquivos .jsx
                 exclude: /node_modules/, // Para não interpretar os arquivos que estão na pasta node_modules
-                use: 'babel-loader', // Integra o babel com o webpack
+                use: {
+                    loader: 'babel-loader', // Integra o babel com o webpack
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean) // Para que o React Refresh funcione.
+                    }
+                },
             },
             {
                 test: /\.scss$/, // Expressão regular para verificar e encontrar arquivos .scss
